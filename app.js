c3d0970b29f8a5d80a12297a24d1e883ee59ba57
yassine <yassinelifefun@gmail.com>
2021-05-16 01:07:18 +0000

    centred button and added color to clicked button

---
+++ app.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $("#number1").click(function() {
+        $("#number1").css('background', 'blue');
+    });
+});
+$(document).ready(function() {
+    $("#number2").click(function() {
+
+        $("#number2").css('background', 'blue');
+    });
+});
